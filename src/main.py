57a6262c6feccacc72a07841bd4001267122ec83
h0bbs99 <jdoakes13@gmail.com>
2024-12-24 12:34:15 -0600

    Ch2 L2 Complete

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from textnode import TextType, TextNode
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.bood.dev")
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
+
 
 main()
